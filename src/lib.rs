6ca1dc0c84c492626e9ee6489bd510e466407294
Ken Chau <kchau@microsoft.com>
2023-02-09 14:52:33 -0800

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 #[macro_use]
 extern crate napi_derive;
 
+use std::collections::HashMap;
 use std::fs;
+use std::thread;
 
 use ignore::overrides::OverrideBuilder;
-use ignore::WalkBuilder;
+use ignore::{DirEntry, WalkBuilder};
 use xxhash_rust::xxh3;
 
 #[napi(object)]
@@ -24,7 +26,7 @@ struct HashGlobOptions {
 pub fn hash_glob(
   globs: Vec<String>,
   maybe_options: Option<PartialHashGlobOptions>,
-) -> Option<Vec<u64>> {
+) -> Option<HashMap<String, u64>> {
   let mut options = HashGlobOptions {
     cwd: ".".to_string(),
     gitignore: true,
@@ -49,23 +51,53 @@ pub fn hash_glob(
   }
 
   if let Ok(overrides) = override_builder.build() {
-    let walker = WalkBuilder::new(cwd)
+    let mut hashes: HashMap<String, u64> = HashMap::new();
+
+    let (tx, rx) = crossbeam_channel::bounded::<(DirEntry, u64)>(100);
+
+    // let collect_thread = thread::spawn(move || {
+    //   for (dir_entry, hash) in rx {
+    //     hashes.insert(dir_entry.path().to_string_lossy().to_string(), hash);
+    //   }
+    // });
+
+    WalkBuilder::new(cwd)
       .overrides(overrides)
       .git_ignore(gitignore)
-      .build_parallel(|entry| {
-        
+      .threads(6)
+      .build_parallel()
+      .run(|| {
+        let tx = tx.clone();
+        Box::new(move |dir_entry_result| {
+          use ignore::WalkState::*;
+          if let Ok(dir_entry) = dir_entry_result {
+            if dir_entry.path().is_file() {
+              let contents = fs::read_to_string(dir_entry.path()).unwrap();
+              tx.send((dir_entry, xxh3::xxh3_64(&contents.as_bytes())));
+            }
+          }
+          Continue
+        })
       });
 
-    let mut hashes: Vec<u64> = Vec::new();
+    // for entry in walker {
+    //   if let Ok(entry) = entry {
+    //     if entry.path().is_file() {
+    //       let contents = fs::read_to_string(entry.path()).unwrap();
+    //       hashes.push(xxh3::xxh3_64(&contents.as_bytes()));
+    //     }
+    //   }
+    // }
 
-    for entry in walker {
-      if let Ok(entry) = entry {
-        if entry.path().is_file() {
-          let contents = fs::read_to_string(entry.path()).unwrap();
-          hashes.push(xxh3::xxh3_64(&contents.as_bytes()));
-        }
-      }
+
+    for (dir_entry, hash) in rx.try_iter().collect() {
+      hashes.insert(dir_entry.path().to_string_lossy().to_string(), hash);
     }
+
+    drop(tx);
+    collect_thread.join().unwrap();
+
+    // let hashes = hashes;
 
     return Some(hashes);
   }
